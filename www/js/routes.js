1469988fbd87da67aab27f7f8f35fe3c6ebbdd24
Agnelo de Santana <agnelosantana2002@gmail.com>
2024-11-04 12:07:38 +0000

    Pending changes exported from your codespace

--- www/js/routes.js
+++ www/js/routes.js
@@ -376,6 +376,26 @@ var app = new Framework7({
 		  },
 		}
 	  },
+	  {
+		path: '/denuncia/',
+		url: 'denuncia.html',
+		animate: false,
+		on: {
+		  pageBeforeIn: function (event, page) {
+		  // fazer algo antes da página ser exibida
+		  $("#menuPrincipal").hide("fast");
+		  },
+		  pageAfterIn: function (event, page) {
+		  // fazer algo depois da página ser exibida
+		  },
+		  pageInit: function (event, page) {
+		  // fazer algo quando a página for inicializada
+		  },
+		  pageBeforeRemove: function (event, page) {
+		  // fazer algo antes da página ser removida do DOM
+		  },
+		}
+	  },
   ],
   // ... other parameters
 });
